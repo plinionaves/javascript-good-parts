7f7effca2704573b15d23f77b6f0b7b4dd860b30
Plínio Naves <pliniopjn@hotmail.com>
2015-06-29 01:00:35 -0300

    Alterações no arquivo recursividade.js

--- capitulo-4-funcoes/recursividade/recursividade.js
+++ capitulo-4-funcoes/recursividade/recursividade.js
@@ -6,4 +6,31 @@ var hanoi = function(disc, src, aux, dst) {
 	}
 };
 
-hanoi(3, 'Origem', 'Auxiliar', 'Destino');
+//hanoi(3, 'Origem', 'Auxiliar', 'Destino');
+
+
+
+// testes
+/*var soma = function(n){
+	if(n === 1){
+		return 1;
+	}
+	return n + soma(n - 1);
+};
+
+console.log(soma(5));*/
+
+/*var soma = function(n){
+	return (n === 1) ? 1 : n+ soma(n-1);
+};
+
+console.log(soma(3));*/
+
+/*var multiplica = function(a, b){
+	return (b === 1) ? a : a + multiplica(a, b - 1);
+};
+console.log(multiplica(9, 3));*/
+
+var divisao = function(a, b){
+	return (b === 1) ? 
+};
